a4259bc322c2a67d4a62278dec2c48524ca18068
MichaelFlops <mwbetts@ucsc.edu>
2014-07-18 02:51:05 -0700

    I broke this shit

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', function (req, res)
 
 app.get('/hacks', function (req, res)
 {
-    res.render('hacks.html');
+    res.render('http://hacks.html');
 });
 app.get('/chessy', function (req, res)
 {
